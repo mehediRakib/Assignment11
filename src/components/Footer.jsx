83bbe3a3b30aca4358411023cb39cc4af68a63f3
Mehedi Hasan <couict6@gmail.com>
2023-10-23 19:18:08 +0600

    add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Footer</h2>
+            <p>This is Footer file</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+
+
+const Project = () => {
+    return (
+        <div>
+            <h2>Project</h2>
+            <p>This is project page</p>
+        </div>
+    );
+};
+
+export default Project;
